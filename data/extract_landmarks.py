67a413003eea2b07d55a4b7375af60b9b0c54c14
jungmoo <jungmoolee@clika.io>
2022-06-18 18:29:04 +0900

    extract_landmarks update (test required)

--- data/extract_landmarks.py
+++ data/extract_landmarks.py
@@ -14,60 +14,20 @@ parser.add_argument('--normalized', default='y', help='y or n, if y record norma
 parser.add_argument('--exclude_z', default='y', help='y or n, if y record x,y coordinates only')
 args = parser.parse_args()
 
+mp_hands = mp.solutions.hands
 
-def extract_landmarks(file_path: str, normalized=True):
-    dir_path, file_name = file_path.rsplit('/', 1)
-    print(dir_path, file_name)  # TODO: logger
-    file_name = file_name.rsplit('.', 1)[0]
 
-    img = cv2.imread(file_path)
-    mp_hands = mp.solutions.hands
+def get_landmarks(img, normalized):
+    with mp_hands.Hands(
+            static_image_mode=True,
+            max_num_hands=1,
+            model_complexity=1,
+            min_detection_confidence=0.7) as hands:
+        img = cv2.flip(img, 1)
+        results = hands.process(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))  # BGR to RGB
 
-    if img is None:
-        # is video
-        cap = cv2.VideoCapture(file_path)
-        if not cap.isOpened():
-            raise IOError(f'could not read file {file_path}')
-
-        while cap.isOpened():
-            ret, frame = cap.read()  # BGR
-
-            if not ret:
-                break
-
-            # MEDIAPIPE
-            with mp_hands.Hands(
-                    static_image_mode=True,
-                    max_num_hands=1,
-                    model_complexity=1,
-                    min_detection_confidence=0.7) as hands:
-                img = cv2.flip(frame, 1)
-                results = hands.process(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))  # BGR to RGB
-
-                print('Handedness:', results.multi_handedness)
-                if not results.multi_hand_landmarks:
-                    continue
-
-                np.save(file_path + file_name + '.npy', results.multi_handedness[0])
-
-        cap.release()
-
-        return ...
-    else:
-        # is img
-        with mp_hands.Hands(
-                static_image_mode=True,
-                max_num_hands=1,
-                model_complexity=1,
-                min_detection_confidence=0.7) as hands:
-            img = cv2.flip(img, 1)
-            results = hands.process(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))  # BGR to RGB
-            print('Handedness:', results.multi_handedness)
-
-            if not results.multi_hand_landmarks:
-                return
+        if not normalized:
             image_height, image_width, _ = img.shape
-            annotated_image = img.copy()
             for hand_landmarks in results.multi_hand_landmarks:  # per hand
                 print(
                     f'Index finger tip coordinates: (',
@@ -75,12 +35,43 @@ def extract_landmarks(file_path: str, normalized=True):
                     f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height})'
                 )
 
+        print('Handedness:', results.multi_handedness)
+        return results.multi_hand_landmarks
 
-    # Closes all the frames
-    # cv2.destroyAllWindows()
+
+def extract_landmarks(file_path: str, normalized=False) -> None:
+    dir_path, file_name = file_path.rsplit('/', 1)
+    print(dir_path, file_name)  # TODO: logger
+    file_name = file_name.rsplit('.', 1)[0]
+
+    img = cv2.imread(file_path)
+
+    if img is None:  # is video
+        cap = cv2.VideoCapture(file_path)
+        if not cap.isOpened():
+            raise IOError(f'could not read file {file_path}')
+
+        landmarks = []
+        while cap.isOpened():
+            ret, frame = cap.read()  # BGR
+            if not ret:
+                break
+
+            results = get_landmarks(frame, normalized)
+            if results is None:
+                continue
+            landmarks.append(results.multi_handedness[0])
+
+        cap.release()
+        np.save(file_path + file_name + '.npy', np.array(landmarks))
+    else:  # is img
+        results = get_landmarks(img, normalized)
+        if results is None:
+            return
+        np.save(file_path + file_name + '.npy', results.multi_handedness[0])
 
 
 df = pd.read_csv(args.data)
 
 for s in df['path']:
-
+    extract_landmarks(file_path=..., normalized=False)
